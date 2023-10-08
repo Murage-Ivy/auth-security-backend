50cdb0006bbc93758cf3e0c419a3fb490af21f00
Murage-Ivy <ivymurage2000@gmail.com>
2023-10-08 22:13:54 +0300

    resolve id error for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+  rescue_from CanCan::AccessDenied, with: :render_unauthorized_entity_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  wrap_parameters format: []
+
   def index
     @comments = Comment.all
     render json: @comments, status: :ok
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.create!(comment_params)
     render json: @comment, status: :created
   end
 
@@ -13,5 +18,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.permit(:request_id, :content)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
